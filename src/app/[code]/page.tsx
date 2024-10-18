a1dbc2d030add3e6356da76bfc852db036cce82a
versecafe <147033096+versecafe@users.noreply.github.com>
2024-10-17 21:29:23 -0700

    update utm

--- src/app/[code]/page.tsx
+++ src/app/[code]/page.tsx
@@ -84,7 +84,7 @@ export default async function Home({
         <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
           <a
             className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org/learn?utm_source=create-next-app"
+            href="https://nextjs.org/learn?utm_source=next-pregen-flags-template"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -114,7 +114,7 @@ export default async function Home({
           </a>
           <a
             className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org?utm_source=create-next-app"
+            href="https://nextjs.org?utm_source=next-pregen-flags-template"
             target="_blank"
             rel="noopener noreferrer"
           >
